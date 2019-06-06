e47cd426ca6c4a8700335a8f91403304d41b44a3
Pascal ALLAU <pascal.allau@formation-occ.com>
2019-06-06 12:10:07 +0200

    adaptation code imgages jpeg

--- boutique.php
+++ boutique.php
@@ -1,17 +1,18 @@
 <?php
-//require_once('includes/connexion.php');
 require_once 'includes/header.php';
 
 $query = "SELECT `auteur`.name, `livre`.titre, `livre`.prix, `livre`.id, `livre`.year FROM `auteur` INNER JOIN `livre` on `auteur`.id = `livre`.auteur_id";
 $result = mysqli_query($link, $query);
-//var_dump($result);
 while ($row = mysqli_fetch_assoc($result)) {
     ?>
-    <h3><?php echo $row['titre'] . '&nbsp'; ?></h3>
-    <h4><?php echo $row['name'] . '&nbsp'; ?></h4>
-    <h4><?php echo $row['year'] . '&nbsp'; ?></h4>
-    <h3><?php echo number_format($row['prix'], 2, ',', ' ') . '&nbsp'; ?> €</h3>
+    <a href="panier.php?action=ajout&amp;l=<?php echo $row['titre']; ?> &amp;q=1&amp;p=<?php echo number_format($row['prix'], 2, ',', ' '); ?>&amp;"><img src="img/<?= $row['id'] ?>.jpeg" alt="image de couverture" width="57" height="80" style = "float:left"></a>
+    <h4><?php echo $row['titre'] . '&nbsp'; ?></h4>
+    <h5><?php echo $row['name'] . '&nbsp'; ?></h5>
+    <h5><?php echo $row['year'] . '&nbsp'; ?></h5>
+    <h4><?php echo number_format($row['prix'], 2, ',', ' ') . '&nbsp'; ?> €</h4>
+    <a href="panier.php?action=ajout&amp;l=<?php echo $row['titre']; ?> &amp;q=1&amp;p=<?php echo number_format($row['prix'], 2, ',', ' '); ?>&amp;">Ajouter au panier</a>
     <br><br>
+    <hr>
     <?php
 }
 require_once 'includes/footer.php';
